bfaa159e30295e7c389d581b824552014ba206e7
Rifat Fazlutdinov <46864568+Rifat-Fazlutdinov@users.noreply.github.com>
2020-03-30 11:31:47 +0500

    Order model was added and congigured in admins.py

--- jolybell/admin.py
+++ jolybell/admin.py
@@ -5,3 +5,4 @@ from .models import *
 admin.site.register(Category)
 admin.site.register(Product)
 admin.site.register(Cart)
+admin.site.register(Order)
--- jolybell/models.py
+++ jolybell/models.py
@@ -24,3 +24,21 @@ class Cart(models.Model):
     def __str__(self):
         string = str(self.user) + "'s cart"
         return string
+
+class Order(models.Model):
+    STATUS_CHOICES = [
+        ('IPM', 'In process of making'),
+        ('INS', 'In stock'),
+        ('IND', 'In delivery'),
+        ('IPO', 'In your post office')
+    ]
+
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    products = models.ManyToManyField(Product)
+    address = models.CharField(max_length=100, null=False)
+    full_price = models.IntegerField(null=False)
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_delivered = models.DateTimeField(null=True)
+    status = models.CharField(max_length=3, choices=STATUS_CHOICES, default='IPM')
+    delivered = models.BooleanField(default=False)
+    
